1aa590e42b6470338fac69b7bd9d722323109fd4
xcharag <oscaraguilarfut@gmail.com>
2026-04-27 11:06:29 -0400

    Restore navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Jugadores from './pages/Jugadores';
 import Hinchas from './pages/Hinchas';
 import Reportes from './pages/Reportes';
@@ -7,12 +8,17 @@ import Reportes from './pages/Reportes';
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/jugadores" replace />} />
-        <Route path="/jugadores" element={<Jugadores />} />
-        <Route path="/hinchas" element={<Hinchas />} />
-        <Route path="/reportes" element={<Reportes />} />
-      </Routes>
+      <div className="app-shell">
+        <Navbar />
+        <main className="app-main">
+          <Routes>
+            <Route path="/" element={<Navigate to="/jugadores" replace />} />
+            <Route path="/jugadores" element={<Jugadores />} />
+            <Route path="/hinchas" element={<Hinchas />} />
+            <Route path="/reportes" element={<Reportes />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
